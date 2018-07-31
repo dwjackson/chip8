164eb27033d4d47af87e56982740111a60f6571e
David Jackson <david@dwjackson.ca>
2018-07-30 20:02:24 -0400

    Add program to hexdump files

---
+++ src/dump8.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define USAGE_FMT "Usage: %s [FILE_NAME]\n"
+#define COLS 79
+
+void hexdump(FILE *fp);
+
+int main(int argc, char *argv[])
+{
+	char *file_name;
+	FILE *fp;
+
+	if (argc < 2) {
+		printf(USAGE_FMT, argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	file_name = argv[1];
+	fp = fopen(file_name, "rb");
+	if (!fp) {
+		perror(file_name);
+		exit(EXIT_FAILURE);
+	}
+
+	hexdump(fp);
+
+	fclose(fp);
+
+	return 0;
+}
+
+void hexdump(FILE *fp)
+{
+	int ch;
+	int i = 0;
+	while ((ch = fgetc(fp)) != EOF) {
+		if (i + 3 > COLS) {
+			i = 0;
+			printf("\n");
+		}
+		printf("%02X ", ch);
+		i += 3;
+	}
+	printf("\n");
+}
