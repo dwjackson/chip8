a5ad564681cbad722961e5d9338b7dd9827955a7
David Jackson <david@dwjackson.ca>
2018-08-04 17:09:27 -0400

    Implement AND in asm8

--- src/encode.c
+++ src/encode.c
@@ -32,6 +32,7 @@ static unsigned short encode_add(struct statement *stmt);
 static unsigned short encode_sub(struct statement *stmt);
 static unsigned short encode_sprite_byte(struct statement *stmt);
 static unsigned short encode_or(struct statement *stmt);
+static unsigned short encode_and(struct statement *stmt);
 
 int encode_statement(struct statement *stmt,
 	struct label labels[MAX_LABELS], size_t num_labels,
@@ -69,6 +70,8 @@ int encode_statement(struct statement *stmt,
 		asm_stmt = encode_add(stmt);
 	} else if (strcmp(ins, "OR") == 0) {
 		asm_stmt = encode_or(stmt);
+	} else if (strcmp(ins, "AND") == 0) {
+		asm_stmt = encode_and(stmt);
 	} else if (strcmp(ins, "SUB") == 0) {
 		asm_stmt = encode_sub(stmt);
 	} else if (strcmp(ins, "DRW") == 0) {
@@ -381,4 +384,24 @@ static unsigned short encode_or(struct statement *stmt)
 	src_byte = strtol(&src[1], NULL, 16);
 
 	return 0x8001 | ((dst_byte << 8) & 0x0F00) | ((src_byte << 4) & 0x00F0);
+}
+
+static unsigned short encode_and(struct statement *stmt)
+{
+	const char *dst;
+	const char *src;
+	unsigned short dst_byte;
+	unsigned short src_byte;
+
+	if (stmt->num_args < 2) {
+		fprintf(stderr, "Too few arguments for OR\n");
+		abort();
+	}
+
+	dst = stmt->args[0];
+	src = stmt->args[1];
+	dst_byte = strtol(&dst[1], NULL, 16);
+	src_byte = strtol(&src[1], NULL, 16);
+
+	return 0x8002 | ((dst_byte << 8) & 0x0F00) | ((src_byte << 4) & 0x00F0);
 }
